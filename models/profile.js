b289c08c9ca1a97f2717b22b6c3084a13d237724
Robert Fridlender <robertfridlender@gmail.com>
2023-02-03 18:52:17 -0500

    fix journeys typo

--- models/profile.js
+++ models/profile.js
@@ -6,7 +6,7 @@ const profileSchema = new Schema({
   name: String,
   photo: String,
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-  journies: [{ type: Schema.Types.ObjectId, ref: 'Journey' }],
+  journeys: [{ type: Schema.Types.ObjectId, ref: 'Journey' }],
   likedPosts: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
   followers: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
   following: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
