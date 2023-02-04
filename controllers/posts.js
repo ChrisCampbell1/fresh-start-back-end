752326a79e82e5e8468e7dc5d7c0e13264fa58f3
Robert Fridlender <robertfridlender@gmail.com>
2023-02-04 18:14:14 -0500

    add posts index

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,17 @@
 import { Post } from '../models/post.js'
 import { Profile } from '../models/profile.js'
 
+const index = async (req, res) => {
+  try {
+    const posts = await Post.find({})
+    posts.sort((a, b) => a.likes.length - b.likes.length)
+    res.status(200).json(posts)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
 // Controller Stub
 
 // const index = async (req, res) => {
@@ -13,5 +24,5 @@ import { Profile } from '../models/profile.js'
 // }
 
 export {
-
+  index
 }
