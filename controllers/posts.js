bdd45085ae41d649e72c35b24a156ba05daf9756
Robert Fridlender <robertfridlender@gmail.com>
2023-02-04 18:07:24 -0500

    stub and connect post router

---
+++ controllers/posts.js
@@ -0,0 +1,17 @@
+import { Post } from '../models/post.js'
+import { Profile } from '../models/profile.js'
+
+// Controller Stub
+
+// const index = async (req, res) => {
+//   try {
+
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).json(err)
+//   }
+// }
+
+export {
+
+}
