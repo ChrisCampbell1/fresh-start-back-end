7ec4bbd95160efbeed0a97655224e2d7ba409e88
leonshiyan <leonshiyan@gmail.com>
2023-02-05 18:45:51 -0800

    add journey photo upload

--- routes/journeys.js
+++ routes/journeys.js
@@ -11,6 +11,7 @@ router.get('/:id', journeysCtrl.show)
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, journeysCtrl.create)
+router.put('/:id/add-photo', checkAuth, journeysCtrl.addPhoto)
 
 router.post('/:id/subscribers', checkAuth, journeysCtrl.addSubscriber)
 router.delete('/:id/subscribers', checkAuth, journeysCtrl.removeSubscriber)
