7c301b5a7e5e15e8b9127cf37a69db5d23e1b97a
Robert Fridlender <robertfridlender@gmail.com>
2023-02-06 15:34:31 -0500

    update profiles index controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,13 +1,15 @@
 import { Profile } from '../models/profile.js'
 import { v2 as cloudinary } from 'cloudinary'
 
-function index(req, res) {
-  Profile.find({})
-  .then(profiles => res.json(profiles))
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+const index = async (req, res) => {
+  try {
+    const profiles = await Profile.find({})
+      .populate('journeys', ['name', 'photo'])
+    res.json(profiles)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 }
 
 const addPhoto = async(req, res) => {
